3dcaeb8e497e30cfcafd2ff4fa65612c10942fb5
Bartłomiej <b.pyzocha@gmail.com>
2022-01-21 17:27:57 +0100

    Initialize project

---
+++ views/transactions.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Transactions</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Check #</th>
+        <th>Description</th>
+        <th>Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+        <!-- YOUR CODE -->
+    </tbody>
+    <tfoot>
+      <tr>
+        <th colspan="3">Total Income:</th>
+        <td><!-- YOUR CODE --></td>
+      </tr>
+      <tr>
+        <th colspan="3">Total Expense:</th>
+        <td><!-- YOUR CODE --></td>
+      </tr>
+      <tr>
+        <th colspan="3">Net Total:</th>
+        <td><!-- YOUR CODE --></td>
+      </tr>
+    </tfoot>
+  </table>
+</body>
+</html>
